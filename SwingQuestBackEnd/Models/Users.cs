8f678a8022c00a2674d3357b7c69e0641b401815
LihinduPerera <lihindu.indudunu.perera@gmail.com>
2025-01-25 03:44:43 +0530

    testApi

---
+++ SwingQuestBackEnd/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SwingQuestBackEnd.Models
+{
+    public class Users
+    {
+        [Key]
+        public int userId { get; set; }
+
+        [Required]
+        public String name { get; set; } = "";
+
+        [Required]
+        public String answer { get; set; } = "";
+    }
+}
